7fa6a1cb6b5553f7fa7a0f771355f81d58fd41c8
dvorakyl <kyle.dvorak@mykenzie.snhu.edu>
2022-03-21 15:11:16 -0500

    Finished task six and seven.

--- IntegrationTests/src/test/java/com/kenzie/unit/two/DepartmentServiceTest.java
+++ IntegrationTests/src/test/java/com/kenzie/unit/two/DepartmentServiceTest.java
@@ -6,24 +6,42 @@ import com.kenzie.unit.two.iam.service.DepartmentService;
 import com.kenzie.unit.two.iam.storage.Storage;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class DepartmentServiceTest {
 
+    @Mock
+    Storage storage;
+
+    @InjectMocks
+    DepartmentService departmentService;
+
+    @BeforeEach
+    public void setup() {
+        openMocks(this);
+    }
+
     @AfterEach
     void afterEach() {
-        assert(false);
+        System.out.println(":)");
     }
 
     @Test
     void createNewDepartment_TASK_7() {
         //GIVEN
-        DepartmentService departmentService = App.departmentService();
+        DepartmentService departmentService = new DepartmentService(storage);
 
         //WHEN
         String departmentName = RandomStringUtils.random(20);
@@ -38,18 +56,19 @@ class DepartmentServiceTest {
     @Test
     void throwExceptionDepartmentNameAlreadyExists_TASK_7() {
         //GIVEN
-        DepartmentService departmentService = App.departmentService();
-
+        DepartmentService departmentService = new DepartmentService(storage);
+        Department departmentMock = mock(Department.class);
         //WHEN
         String departmentName = RandomStringUtils.random(20);
 
         CreateDepartmentRequest createDepartmentRequest = new CreateDepartmentRequest();
         createDepartmentRequest.setDepartmentName(departmentName);
-
+        when(storage.getDepartmentByName(createDepartmentRequest.getDepartmentName())).thenReturn(null);
         departmentService.createDepartment(createDepartmentRequest);
 
 
         //THEN
+        when(storage.getDepartmentByName(createDepartmentRequest.getDepartmentName())).thenReturn(departmentMock);
         assertThrows(IllegalArgumentException.class,
                 () -> departmentService.createDepartment(createDepartmentRequest));
     }
@@ -57,37 +76,42 @@ class DepartmentServiceTest {
     @Test
     void getDepartmentByName_TASK_7(){
         //GIVEN
-        DepartmentService departmentService = App.departmentService();
+        DepartmentService departmentService = new DepartmentService(storage);
 
         //WHEN
         String departmentName = RandomStringUtils.random(20);
 
         CreateDepartmentRequest createDepartmentRequest = new CreateDepartmentRequest();
         createDepartmentRequest.setDepartmentName(departmentName);
-
+        when(storage.getDepartmentByName(createDepartmentRequest.getDepartmentName())).thenReturn(null);
         Department department = departmentService.createDepartment(createDepartmentRequest);
 
 
         //THEN
+        when(storage.getDepartmentByName(createDepartmentRequest.getDepartmentName())).thenReturn(department);
         Department queriedDepartment = departmentService.getDepartmentByName(departmentName);
         assertTrue(department.getId().equals(queriedDepartment.getId()));
+        verify(storage, times(2)).getDepartmentByName(createDepartmentRequest.getDepartmentName());
     }
 
     @Test
     void getAllDepartments(){
         //GIVEN
-        DepartmentService departmentService = App.departmentService();
-
+        DepartmentService departmentService = new DepartmentService(storage);
+        List<Department> departmentsMock = new ArrayList<>();
         //WHEN
         String departmentName = RandomStringUtils.random(20);
 
         CreateDepartmentRequest createDepartmentRequest = new CreateDepartmentRequest();
         createDepartmentRequest.setDepartmentName(departmentName);
-
+        when(storage.getDepartmentByName(createDepartmentRequest.getDepartmentName())).thenReturn(null);
         Department department = departmentService.createDepartment(createDepartmentRequest);
 
         //THEN
+        departmentsMock.add(department);
+        when(storage.getDepartments()).thenReturn(departmentsMock);
         List<Department> departments = departmentService.getDepartments();
         assertTrue(departments.contains(department));
+        verify(storage).getDepartments();
     }
 }
--- IntegrationTests/src/test/java/com/kenzie/unit/two/UserRoleCustomTest.java
+++ IntegrationTests/src/test/java/com/kenzie/unit/two/UserRoleCustomTest.java
@@ -1,13 +1,42 @@
 package com.kenzie.unit.two;
 
+import com.kenzie.unit.two.employee.lambda.models.ViewEmployeePayCheckRequest;
+import com.kenzie.unit.two.employee.service.UserOrRoleNotFoundException;
+import com.kenzie.unit.two.iam.service.UserRoleService;
+import com.kenzie.unit.two.iam.storage.Storage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.MockitoAnnotations.openMocks;
+
 
 public class UserRoleCustomTest {
+
+    @Mock
+    Storage storage;
+
+    @InjectMocks
+    UserRoleService userRoleService;
+
+    @BeforeEach
+    public void setup() {
+        openMocks(this);
+    }
+
     @Test
     void missingUserRoleThrowsException_TASK_6() {
         // TODO - write this test
         // Hint - Look at the test in missingUserRoleThrowsException_TASK_2.
         // This will demonstrate how to assert an exception has been thrown
-        assert(false);
+
+        assertThrows(UserOrRoleNotFoundException.class,
+                () -> this.userRoleService.doesUserHaveRole(null, null));
+
     }
 }
